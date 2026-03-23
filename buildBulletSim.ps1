7a18d9138348e3f2a62b6ba97e0324f15d6d63ff
Manfred Aabye <zainhofer@gmx.de>
2026-03-23 09:42:30 +0100

    Delete buildBulletSim.ps1

--- buildBulletSim.ps1
+++
@@ -1,7 +0,0 @@
-
-$BULLETVERSION = Get-Content -Path .\lib\VERSION
-$BULLETSIMVERSION = Get-Content -Path .\VERSION
-
-Set-Item -Path Env:CL -Value "/DBULLETVERSION#$BULLETVERSION /DBULLETSIMVERSION#$BULLETSIMVERSION"
-
-msbuild -p:Configuration=Release BulletSim.sln
